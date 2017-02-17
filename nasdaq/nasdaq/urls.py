b204f93cfea0f4d15973e1fd3140e6a896be0a08
Nate <nate@flashframe.io>
2017-02-16 19:00:52 -0600

    Fixed css in admin

--- nasdaq/nasdaq/urls.py
+++ nasdaq/nasdaq/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^stockmarket/', include('stockmarket.urls', namespace='stockmarket')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
