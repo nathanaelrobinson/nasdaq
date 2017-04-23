b75e59c8aa81e38d9e7d1b71650d83a59aefb822
Nate <nate@flashframe.io>
2017-04-23 11:02:26 -0500

    Added snippets API tutorial as separate URL view

---
+++ nasdaq/snippets/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+	url(r'^$', views.SnippetList.as_view()),
+	url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
